40d3052f76cbccf3f629d61c190237cae1b013d1
bklynate <nathanielcfa@gmail.com>
2015-08-30 12:46:23 -0400

    Added password migration to the user db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804020110) do
+ActiveRecord::Schema.define(version: 20150830162808) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20150804020110) do
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password"
   end
 
 end

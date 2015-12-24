7cc959391405710a55ecf9262c41fa4ca3d29d8e
bklynate <nathanielcfa@gmail.com>
2015-12-24 16:12:22 -0500

    error can't get category show page to display all posts associated

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,7 +15,7 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @category = Category.find_by(params[:id])
+    @category = Category.find(params[:id])
   end
 
   private
